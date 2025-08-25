63005449211eae474476aae1c98bb27ab70e3cc5
ali <alibakhshifccc@gmail.com>
2025-08-25 10:49:51 +0330

    feat:impore the mode

---
+++ app/app/core/security.py
@@ -0,0 +1,33 @@
+from datetime  import datetime, timedelta 
+from hashlib import sha256
+from typing import Any
+import jwt
+import bcrypt
+from fastapi.security import HTTPBasic
+from app.core.config import settings
+
+
+base_securaty = HTTPBasic(auto_error=False)   
+
+def verify_password(plain_password: str, hashed_password: str)-> bool:
+    return hashed_password == sha256(plain_password.encode()).hexdigest()
+
+
+def get_password_hash(password: str)-> str:
+    return sha256(password.encode()).hexdigest()
+
+
+class JWTHandler:    
+    secret_key = settings.SECRET_KEY
+    algorithm = settings.JWT_ALGORITHM
+    access_token_expire = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    refresh_token_expire = settings.REFRESH_TOKEN_EXPIRE_MINUTES
+
+
+    @staticmethod
+    def encode(payload: dict[str, Any])-> str:
+        expire = datetime.utcnow() + timedelta(minutes=JWTHandler.access_token_expire)
+        payload.update({"exp": expire})
+        return jwt.encode(
+            payload, JWTHandler.secret_key, algorithm=JWTHandler.algorithm
+        )
