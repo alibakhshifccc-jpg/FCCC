7b1745603519f7fbcdcbed23cdfd790f3f4a7a07
ali <alibakhshifccc@gmail.com>
2025-08-25 09:54:37 +0330

    feat:Make the Config about app

---
+++ app/app/core/config.py
@@ -0,0 +1,19 @@
+from typing import Any
+from pydantic import (
+    AnyHttpUrl,
+    EmailStr,
+    PostgresDsn,
+    RedisDsn,
+    field_validator
+)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+REFRESH_TOKEN_BLOCKLIST_KEY = "refresh_token_blocklist:{token}"
+ACCESS_TOKEN_BLOCKLIST_KEY = "access_token_blocklist:{token}"
+
+
+
+
+class AsyncPostgresDsn(PostgresDsn):
+    allowed_schemes = {"postgres+asyncpg", "postgresql+asyncpg"}
