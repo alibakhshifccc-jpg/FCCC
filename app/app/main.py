bf47b880fa8d6d1f1a5b1883a41eae6c1db15cad
ali <alibakhshifccc@gmail.com>
2025-08-25 09:48:16 +0330

    feat:improve the lifespan

--- app/app/main.py
+++ app/app/main.py
@@ -1,13 +1,13 @@
 import os 
 import logging, sys , os
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, requests, Response 
+from fastapi import FastAPI, Request, Response 
 from fastapi.middleware import Middleware
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session1
+from sqlalchemy.orm import Session
 from starlette.middleware.cors import CORSMiddleware
-
+from cache import Cache
 
 current_dir = os.path.dirname(__file__)
 static_dir = os.path.join(current_dir, "static")
@@ -31,4 +31,17 @@ init_logger()
 
 
 
+def make_middleware()-> list[Middleware]:
+    pass 
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis_cache = Cache()
+    url = str 
+    await redis_cache.init(
+        host_url=url,
+        prefix="api-cache",
+        response_header="X-API-Cache",
+        ignore_arg_types=[Request, Response, Session, AsyncSession]
+    )
