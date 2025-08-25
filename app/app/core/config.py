6cf5a0e049ddefb6bf535614badd521c1523750f
ali <alibakhshifccc@gmail.com>
2025-08-25 10:04:41 +0330

    feat:imporve the setting class

--- app/app/core/config.py
+++ app/app/core/config.py
@@ -19,6 +19,15 @@ class AsyncPostgresDsn(PostgresDsn):
     allowed_schemes = {"postgres+asyncpg", "postgresql+asyncpg"}
 
 
-class setting:
-    def __init__(self)->Noen:
+class setting(BaseSettings):
+    def __init__(self)-> None:
         pass
+
+    PROJECT_NAME: str
+    API_V1_STR: str = "/api/v1"
+    DEBUG: bool = False
+    SECRET_KEY: str
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] | str = []
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
+    JWT_ALGORITHM: str = "HS256" 
