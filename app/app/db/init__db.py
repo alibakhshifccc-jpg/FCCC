8faf2e28677decdd14eaa060b9c1e2e53ef9eb23
ali <alibakhshifccc@gmail.com>
2025-08-28 12:07:17 +0330

    feat:fix

--- app/app/db/init__db.py
+++ app/app/db/init__db.py
@@ -12,7 +12,16 @@ from app.app import schemas, crud
 logger = logging.getLogger(__name__)
 
 
-async def create_super_admin(db: AsyncSession)-> None:
+async def create_super_admin(db: AsyncSession) -> None:
     user = await crud.user.get_by_email(db=db, email=settings.FIRST_SUPERUSER)
     if not user:
-        user = schemas.U
+        user = schemas.UserCreate(
+            email=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True,
+        )
+        await crud.user.create(db=db, obj_in=user)
+
+
+
+        
