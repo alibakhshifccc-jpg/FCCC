d23ab524be2da9c2fbb8fd3c6be60c7e2af6d311
ali <alibakhshifccc@gmail.com>
2025-08-31 09:50:33 +0330

    feat:fix all

--- app/app/schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-from .msg import *
-from .request_log import *
-from .token import *
-from .user import *
-from .health import *
-from .vertex import *
--- app/app/schemas/vertex.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-
-
-class Vertex(BaseModel):
-    pass
