d4d9393dee84f95e24b9070fe5b16c6536f1bbfe
ali <alibakhshifccc@gmail.com>
2025-08-31 08:12:10 +0330

    feat:tix

---
+++ app/core/middleware/time_logger.py
@@ -0,0 +1,17 @@
+from time import time
+import logging
+from fastapi import Response, Request
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from app.app.core.config import settings
+from typing import Callable, Awaitable
+
+logger = logging.getLogger(__name__)
+
+
+class TimeLoggerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint, conf = settings) -> Response:
+        start_time = time()
+        response = await call_next(request)
+        logger.info(f"[{request.method}:{request.url} in {time() - start_time:.3f} ms]")
+
+        return response
