a10a65350d476d9e642f909de7f77c2f442d698c
ali <alibakhshifccc@gmail.com>
2025-09-04 10:26:30 +0330

    feat:impprove the model

--- app/models/test_yolo.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-model = YOLO("yolov8n.pt")  # مدل از پیش آموزش‌دیده
-image = cv2.imread("sample.jpg")
-results = model(image)
-results.show()  # نمایش تصویر با کادرهای تشخیص
